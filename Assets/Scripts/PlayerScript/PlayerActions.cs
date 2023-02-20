280bbf1e58c644dee7ef1082763e2503ad89cd70
Roussel <nicolas-roussel@hotmail.fr>
2023-02-20 16:58:52 +0100

    modif Jbonk + crea script PlayerActions

---
+++ Assets/Scripts/PlayerScript/PlayerActions.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActions : MonoBehaviour
+{
+
+    //ABSORBER:, pression d'une touche, active pendant 1 sec l'absorbtion 
+    // Si un objet avec variable absorbable à true, et pas null dans son script (ou autre methodes de verif),
+    // genre un tableau d'id dans un script manager
+    //Absorbe l'objet (l'objet est detruit)
+    // on ne peu plus absorber avec cette touche
+    // si on appui sur la touche en renvoi l'objet stocké
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerScript/PlayerMove.cs
+++ Assets/Scripts/PlayerScript/PlayerMove.cs
@@ -28,12 +28,23 @@ public class PlayerMove : MonoBehaviour
     //Tant que l'on reste en collision avec un autre objet.
     void OnCollisionStay2D(Collision2D _collision)
     {
-        if(_collision.gameObject.tag == "sol")
+        if(_collision.gameObject.CompareTag("sol"))
         {
             isGround = true;
         } 
     }
-   
+
+    void OnCollisionEnter2D(Collision2D _collision)
+    {
+        //Declenche un JBONK, en colliion avec un obstacle, dans les airs
+        if(_collision.gameObject.tag == "obstacle")
+        {
+            body.velocity = new Vector2(0f, body.velocity.y);
+            float JbonkForce = 2f;
+            JbonkAerien(JbonkForce, faceRight);
+        }
+    }
+
 
     void Deplacements()
     {       
@@ -43,35 +54,18 @@ public class PlayerMove : MonoBehaviour
 
         //definir la velocite du personnage
         //Je determine la direction du player grace au bool faceRight
-        float velocite = deplacementsHorizontaux * playerSpeed * Time.deltaTime;
+        float velocite = deplacementsHorizontaux * playerSpeed;
         Vector2 playerDirection = faceRight ? Vector2.right : Vector2.left;
-       
 
-        // Obtenir les dimensions du sprite
-        // Déterminer la position de départ du raycast
-        // lancer un raycast dans le direction du personnage
-        // Dessiner le Raycast dans la scène
-        Vector2 spriteDimensions = GetComponent<Collider2D>().bounds.size;
-        Vector2 raycastStart = new Vector2(transform.position.x + (faceRight ? spriteDimensions.x / 2f + 0.01f : -spriteDimensions.x / 2f - 0.01f), transform.position.y);
-        RaycastHit2D hit = Physics2D.Raycast(raycastStart, playerDirection, 0.1f);
-        Debug.DrawRay(raycastStart, playerDirection * 0.1f, Color.red);
+        //pour detecter un objet avec un raycast et faire un  JBONK
+        //PlayerAerienJbonkDetection(playerDirection);
 
-        //DEPLACEMENTS
-        if (hit.collider == null || !hit.collider.gameObject.CompareTag("obstacle")) //Si il n'y a pas d'obstacle
+        //DEPLACEMENTS        
+        if (canMove && deplacementsHorizontaux != 0) 
         {
-            if (canMove && deplacementsHorizontaux != 0) 
-            {
-                body.velocity = new Vector2(velocite, body.velocity.y);
-                //body.AddForce(new Vector2(velocite, body.velocity.y), ForceMode2D.Force);
-            }
-        } 
-        else // Si il y a un obstacle
-        {
-            body.velocity = new Vector2(0f, body.velocity.y);
-            // JBONK AERIEN
-            float JbonkForce = 2f;
-            JbonkAerien(JbonkForce);
-        }    
+            body.velocity = new Vector2(velocite, body.velocity.y);
+            //body.AddForce(new Vector2(velocite, body.velocity.y), ForceMode2D.Force);
+        }          
  
     }
 
@@ -80,9 +74,29 @@ public class PlayerMove : MonoBehaviour
         if (Input.GetAxisRaw("Saut") > 0 && isGround)
         {
             // Applique une force de saut sur le corps rigide en fonction de la durée du saut
-            body.AddForce(Vector2.up * jumpForce * Time.deltaTime);
+            body.AddForce(Vector2.up * jumpForce);
             isGround = false;                    
         }
+    }
+
+    void PlayerAerienJbonkDetection(Vector2 _playerDirection)
+    {
+        // Obtenir les dimensions du sprite
+        // Déterminer la position de départ du raycast
+        // lancer un raycast dans le direction du personnage
+        // Dessiner le Raycast dans la scène
+        Vector2 spriteDimensions = GetComponent<Collider2D>().bounds.size;
+        Vector2 raycastStart = new Vector2(transform.position.x + (faceRight ? spriteDimensions.x / 2f + 0.01f : -spriteDimensions.x / 2f - 0.01f), transform.position.y);
+        RaycastHit2D hit = Physics2D.Raycast(raycastStart, _playerDirection, 0.1f);
+        Debug.DrawRay(raycastStart, _playerDirection * 0.1f, Color.red);
+        
+        if (hit.collider != null && hit.collider.gameObject.CompareTag("obstacle")) //Si il n'y a pas d'obstacle
+        {
+            body.velocity = new Vector2(0f, body.velocity.y);
+            // JBONK AERIEN
+            float JbonkForce = 2f;
+            JbonkAerien(JbonkForce, faceRight);
+        } 
     }
 
     //FONCTION qui appelle un flip du sprite en cas de changement de direction du joueur.
@@ -106,19 +120,19 @@ public class PlayerMove : MonoBehaviour
     }
 
     //FONCTION permettant de faire Jbonk le perso si il est dans les airs, en fonction de la direction dans laquelle il regarde
-    void JbonkAerien(float _JbonkForce)
+    void JbonkAerien(float _JbonkForce, bool _faceRight)
     {
         if (!isGround)
         {            
-            StartCoroutine(JbonkAerienCoroutine(_JbonkForce));            
+            StartCoroutine(JbonkAerienCoroutine(_JbonkForce, _faceRight));            
         }
     }
 
     //Coroutine permettant de faire Jbonk le perso si il est dans les airs, en fonction de la direction dans laquelle il regarde
-    IEnumerator JbonkAerienCoroutine(float _jbonkForce)
+    IEnumerator JbonkAerienCoroutine(float _jbonkForce, bool _faceRight)
     {
         canMove = false;
-        if (faceRight)
+        if (_faceRight)
         {
             body.AddForce(Vector2.left * _jbonkForce, ForceMode2D.Impulse);
             yield return new WaitUntil(() => isGround);
