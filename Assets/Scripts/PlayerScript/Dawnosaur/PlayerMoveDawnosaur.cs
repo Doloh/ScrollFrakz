421cf97710a3d24628e70335d8deb29ca4ee07e4
Vincent Dilly <vdilly.prod@gmail.com>
2023-02-21 18:56:06 +0100

    init : create player prefab and script

---
+++ Assets/Scripts/PlayerScript/Dawnosaur/PlayerMoveDawnosaur.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveDawnosaur : MonoBehaviour
+{
+
+    void Awake()
+    {
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    //MOVEMENT METHODS
+}
